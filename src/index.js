994c32c34d5b578dff8df4036eb5f3b20998a7a0
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-07-18 13:58:02 +0200

    feat: catch all unhandled errors

--- src/index.js
+++ src/index.js
@@ -16,71 +16,78 @@ const mnemonicKey = new MnemonicKey({
 })
 
 export async function run() {
-  const gasPrices = await getGasPrices()
-  const gasPricesCoins = new Coins(gasPrices)
+  try {
+    const gasPrices = await getGasPrices()
+    const gasPricesCoins = new Coins(gasPrices)
 
-  const lcdClient = new LCDClient({
-    chainID: 'pisco-1',
-    gasAdjustment: '1.5',
-    gasPrices: gasPricesCoins,
-    isClassic: false,
-    URL: 'https://pisco-lcd.terra.dev',
-  })
+    const lcdClient = new LCDClient({
+      chainID: 'pisco-1',
+      gasAdjustment: '1.5',
+      gasPrices: gasPricesCoins,
+      isClassic: false,
+      URL: 'https://pisco-lcd.terra.dev',
+    })
 
-  const wallet = lcdClient.wallet(mnemonicKey)
+    const wallet = lcdClient.wallet(mnemonicKey)
 
-  if (DELEGATOR_ADDRESS.length === 0) {
-    DELEGATOR_ADDRESS = wallet.key.accAddress
-    VALIDATOR_ADDRESS = (await lcdClient.staking.delegations(DELEGATOR_ADDRESS))[0][0].validator_address
-  }
+    if (DELEGATOR_ADDRESS.length === 0) {
+      DELEGATOR_ADDRESS = wallet.key.accAddress
+      VALIDATOR_ADDRESS = (await lcdClient.staking.delegations(DELEGATOR_ADDRESS))[0][0].validator_address
+    }
 
-  const currentStakeAmount = (await lcdClient.staking.delegations(DELEGATOR_ADDRESS))[0][0].balance.amount
-  const currentStakeAmountLog = numeral(currentStakeAmount / 1000000)
-    .format('0,0.00')
-    .padStart(12, ' ')
-  B.info(`Current Stake:    ${currentStakeAmountLog} LUNA`)
-
-  const pendingRewardCoin = (await lcdClient.distribution.rewards(DELEGATOR_ADDRESS)).rewards[VALIDATOR_ADDRESS].get(
-    'uluna',
-  )
-  if (pendingRewardCoin === undefined) {
-    await waitFor(10)
-    run()
-
-    return
-  }
-  const pendingRewardAmount = pendingRewardCoin.amount
-  const pendingRewardAmountLog = numeral(pendingRewardAmount / 1000000)
-    .format('0,0.00')
-    .padStart(12, ' ')
-  B.info(`Pending Reward:   ${pendingRewardAmountLog} LUNA`)
-
-  if (pendingRewardAmount >= REWARD_AMOUNT_THRESHOLD) {
-    const pendingRewardWithdrawalMessage = new MsgWithdrawDelegatorReward(DELEGATOR_ADDRESS, VALIDATOR_ADDRESS)
-    const pendingRewardWithdrawalTx = await wallet.createAndSignTx({ msgs: [pendingRewardWithdrawalMessage] })
-    // eslint-disable-next-line no-unused-vars
-    const pendingRewardWithdrawalTxResult = await lcdClient.tx.broadcast(pendingRewardWithdrawalTx)
-
-    await waitFor(10)
-    B.success(`Withdrawn Reward: ${pendingRewardAmountLog} LUNA`)
-
-    const balanceAmount = (await lcdClient.bank.balance(DELEGATOR_ADDRESS))[0].get('uluna').amount
-    const newStakeAmount = Math.floor((balanceAmount - 500000) / 1000000) * 1000000
-
-    const newStakeCoin = new Coin('uluna', newStakeAmount)
-    const newStakeMessage = new MsgDelegate(DELEGATOR_ADDRESS, VALIDATOR_ADDRESS, newStakeCoin)
-    const newStakeTx = await wallet.createAndSignTx({ msgs: [newStakeMessage] })
-    // eslint-disable-next-line no-unused-vars
-    const newStakeTxResult = await lcdClient.tx.broadcast(newStakeTx)
-
-    const newStakeAmountLog = numeral(newStakeAmount / 1000000)
+    const currentStakeAmount = (await lcdClient.staking.delegations(DELEGATOR_ADDRESS))[0][0].balance.amount
+    const currentStakeAmountLog = numeral(currentStakeAmount / 1000000)
       .format('0,0.00')
       .padStart(12, ' ')
-    B.success(`New Stake:        ${newStakeAmountLog} LUNA`)
-  }
+    B.info(`Current Stake:    ${currentStakeAmountLog} LUNA`)
 
-  await waitFor(10)
-  run()
+    const pendingRewardCoin = (await lcdClient.distribution.rewards(DELEGATOR_ADDRESS)).rewards[VALIDATOR_ADDRESS].get(
+      'uluna',
+    )
+    if (pendingRewardCoin === undefined) {
+      await waitFor(10)
+      run()
+
+      return
+    }
+    const pendingRewardAmount = pendingRewardCoin.amount
+    const pendingRewardAmountLog = numeral(pendingRewardAmount / 1000000)
+      .format('0,0.00')
+      .padStart(12, ' ')
+    B.info(`Pending Reward:   ${pendingRewardAmountLog} LUNA`)
+
+    if (pendingRewardAmount >= REWARD_AMOUNT_THRESHOLD) {
+      const pendingRewardWithdrawalMessage = new MsgWithdrawDelegatorReward(DELEGATOR_ADDRESS, VALIDATOR_ADDRESS)
+      const pendingRewardWithdrawalTx = await wallet.createAndSignTx({ msgs: [pendingRewardWithdrawalMessage] })
+      // eslint-disable-next-line no-unused-vars
+      const pendingRewardWithdrawalTxResult = await lcdClient.tx.broadcast(pendingRewardWithdrawalTx)
+
+      await waitFor(10)
+      B.success(`Withdrawn Reward: ${pendingRewardAmountLog} LUNA`)
+
+      const balanceAmount = (await lcdClient.bank.balance(DELEGATOR_ADDRESS))[0].get('uluna').amount
+      const newStakeAmount = Math.floor((balanceAmount - 500000) / 1000000) * 1000000
+
+      const newStakeCoin = new Coin('uluna', newStakeAmount)
+      const newStakeMessage = new MsgDelegate(DELEGATOR_ADDRESS, VALIDATOR_ADDRESS, newStakeCoin)
+      const newStakeTx = await wallet.createAndSignTx({ msgs: [newStakeMessage] })
+      // eslint-disable-next-line no-unused-vars
+      const newStakeTxResult = await lcdClient.tx.broadcast(newStakeTx)
+
+      const newStakeAmountLog = numeral(newStakeAmount / 1000000)
+        .format('0,0.00')
+        .padStart(12, ' ')
+      B.success(`New Stake:        ${newStakeAmountLog} LUNA`)
+    }
+
+    await waitFor(10)
+    run()
+  } catch (err) {
+    B.error(String(err))
+
+    await waitFor(60)
+    run()
+  }
 }
 
 run()
